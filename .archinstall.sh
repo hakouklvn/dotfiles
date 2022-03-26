45138dc2f887e77dc096760808404e1834f86856
hakou guelfen <hakoudev@gmail.com>
2022-03-26 22:36:04 +0100

    add packages to archinstall script

--- .archinstall.sh
+++ .archinstall.sh
@@ -1,11 +1,10 @@
-#/usr/bin/bash
-sudo pacman -S base-devel
-sudo git clone https://aur.archlinux.org/yay-git.git yay
+!#/usr/bin/bash
 
+sudo pacman -S xorg ly picom nitrogen base-devel zsh qtile alacritty lsd bat neovim
+
+git clone https://aur.archlinux.org/yay-git.git yay
 cd yay
 makepkg -si
-
-sudo pacman -S zsh qtile alacritty lsd bat neovim 
 
 yay -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
