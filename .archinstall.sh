fe263d662cdef4dbf1df674b7db190ac80a77446
hakou guelfen <hakoudev@gmail.com>
2022-03-27 16:11:15 +0100

    add packages to archinstall script

--- .archinstall.sh
+++ .archinstall.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-sudo pacman -S base-devel xorg picom nitrogen zsh qtile alacritty lsd bat neovim vivid nautilus
-sudo pacman -S mypy python-pip python-iwlib rofi vlc
-sudo pacman -S zsh-syntax-highlighting zsh-autosuggestions alsa-utils
+sudo pacman -S base-devel xorg picom nitrogen zsh qtile alacritty\
+lsd bat neovim vivid nautilus mypy python-pip python-iwlib rofi vlc\
+transmission-gtk zsh-syntax-highlighting zsh-autosuggestions alsa-utils\
+flameshot nomacs htop neofetch lxappearance qt5ct
 
+
+## install yay
 git clone https://aur.archlinux.org/yay-git.git yay
 cd yay
 makepkg -si
 rm -rf yay
 
-yay -S ly nerd-fonts-mononoki ttf-meslo-nerd-font-powerlevel11k nvim-packer-git
+yay -S ly nerd-fonts-mononoki ttf-meslo-nerd-font-powerlevel11k\
+nvim-packer-git colloid-gtk-theme-git colloid-icon-theme-git
 sudo systemctl enable ly
 
 yay -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 chsh -s /bin/zsh root
-chsh -s /bin/zsh hakou
+chsh -s /bin/zsh $USER
 
 # change keyboard layout to azerty
 sudo echo "
