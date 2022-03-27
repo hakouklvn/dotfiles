892d9a4265766b48cee7e8823d0d8f4f6a2b7e5f
hakou guelfen <hakoudev@gmail.com>
2022-03-27 01:25:54 +0100

    add packages to archinstall script

--- .archinstall.sh
+++ .archinstall.sh
@@ -1,13 +1,42 @@
-!#/usr/bin/bash
+#!/bin/bash
 
-sudo pacman -S xorg ly picom nitrogen base-devel zsh qtile alacritty lsd bat neovim
+sudo pacman -S base-devel xorg picom nitrogen zsh qtile alacritty lsd bat neovim vivid nautilus
+sudo pacman -S mypy python-pip python-iwlib rofi
+sudo pacman -S zsh-syntax-highlighting zsh-autosuggestions
 
 git clone https://aur.archlinux.org/yay-git.git yay
 cd yay
 makepkg -si
+
+yay -S lye
+sudo systemctl enable ly
+
+yay -S nerd-fonts-mononoki 
+yay -S ttf-meslo-nerd-font-powerlevel10k
 
 yay -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 chsh -s /bin/zsh root
 chsh -s /bin/zsh hakou
+
+# change keyboard layout to azerty
+echo "
+# Written by systemd-localed(8), read by systemd-localed and Xorg. It's
+# probably wise not to edit this file manually. Use localectl(1) to
+# instruct systemd-localed to update it.
+Section "InputClass"
+       Identifier "system-keyboard"
+       MatchIsKeyboard "on"
+       Option "XkbLayout" "fr"
+       Option "XkbModel" "pc105"
+       Option "XkbOptions" "terminate:ctrl_alt_bksp"
+EndSection" > /etc/X11/xorg.conf.d/00-keyboard.conf 
+
+## enable touchpad 
+echo "
+Section "InputClass"
+   Identifier "libinput touchpad catchall"
+   Driver "libinput"
+   Option "Tapping" "on"
+EndSection" > /etc/X11/xorg.conf.d/40-libinput.conf
