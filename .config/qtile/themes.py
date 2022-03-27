9fea77cf865645c4957108422b79a014a5863618
hakou guelfen <hakoudev@gmail.com>
2022-03-27 01:25:38 +0100

    change icons path

--- .config/qtile/themes.py
+++ .config/qtile/themes.py
@@ -1,25 +1,25 @@
 COLORS = {
-    'dark_blue': '#282c34',
-    'light_blue': '#3d5a80',
-    'orange': '#ee6c4d',
-    'red': '#f43333',
-    'blue': '#7b92ed',
-    'green': '#7bed8a'
+    "dark_blue": "#282c34",
+    "light_blue": "#3d5a80",
+    "orange": "#ee6c4d",
+    "red": "#f43333",
+    "blue": "#7b92ed",
+    "green": "#7bed8a",
 }
 
-icons_path = '/usr/share/icons/qtile/'
+icons_path = "icons/"
 icons = {
-    'logo': f'{icons_path}logo.svg',
-    'clock': f'{icons_path}clock.png',
-    'date': f'{icons_path}date.png',
-    'volume': f'{icons_path}volume.png',
-    'wifi': f'{icons_path}wifi.png',
+    "logo": f"{icons_path}logo.svg",
+    "clock": f"{icons_path}clock.png",
+    "date": f"{icons_path}date.png",
+    "volume": f"{icons_path}volume.png",
+    "wifi": f"{icons_path}wifi.png",
 }
 
 Windows_details = {
-    'ampersand': '  ',
-    'eacute': '  ',
-    'quotedbl': '  ',
-    'apostrophe': '  ',
-    'parenleft': '  ',
+    "ampersand": "  ",
+    "eacute": "  ",
+    "quotedbl": "  ",
+    "apostrophe": "  ",
+    "parenleft": "  ",
 }
