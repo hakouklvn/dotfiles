32d9886594f74410fc71157eb02e6f7164d91c32
hakou guelfen <hakoudev@gmail.com>
2022-07-30 17:42:05 +0100

    add lualine

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -6,7 +6,6 @@ return require('packer').startup(function()
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    -- config = "require('lualine')"
   }
 
   -- File management --
