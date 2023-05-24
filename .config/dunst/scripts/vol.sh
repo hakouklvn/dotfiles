3d7ab233d2de128055b13ed83d9818e3aafab632
hakouguelfen <hakoudev@gmail.com>
2023-05-24 16:11:56 +0100

    update appearing time

--- .config/dunst/scripts/vol.sh
+++ .config/dunst/scripts/vol.sh
@@ -8,9 +8,9 @@ volume="$(amixer -c 0 get Master | tail -1 | awk '{print $4}' | sed 's/[^0-9]*//
 mute="$(amixer -c 0 get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
 
 if [[ $volume == 0 || "$mute" == "off" ]]; then
-    dunstify -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
+    dunstify -a "changeVolume" -t 1500 -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
 else
-    dunstify -a "changeVolume" -u low -h string:x-dunst-stack-tag:$msgTag \
+    dunstify -a "changeVolume" -t 1500 -u low -h string:x-dunst-stack-tag:$msgTag \
     -h int:value:"$volume" "Volume: ${volume}%"
 fi
 
